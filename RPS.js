18a918e40695362c689bd3c9cc83fdbf1e728d8b
luke sill <lukesill@icloud.com>
2023-01-19 23:49:30 -0500

    Began playRound function

---
+++ RPS.js
@@ -0,0 +1,17 @@
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+}
+ 
+const playerSelection = "rock";
+
+
+const computerSelection = getComputerChoice();
+
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+
